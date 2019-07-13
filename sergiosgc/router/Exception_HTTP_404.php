fc55795970687515475ed14a22d8f4036878290a
Sérgio Carvalho <sergio.carvalho@sooma.com>
2019-07-13 17:33:46 +0100

    Have 404 exception actually output a 404 HTTP result

--- sergiosgc/router/Exception_HTTP_404.php
+++ sergiosgc/router/Exception_HTTP_404.php
@@ -3,11 +3,12 @@ namespace sergiosgc\router;
 
 class Exception_HTTP_404 extends Exception {
     public function __construct($url = null) {
+        header('HTTP/1.0 404 Not Found');
         if (is_null($url)) {
             $url = sprintf("%s://%s%s", isset($_REQUEST['HTTPS']) && $_REQUEST['HTTPS'] == 'on' ? 'https' : 'http', $_SERVER['HTTP_HOST'], $_SERVER['REQUEST_URI']);
         }
         $this->url = $url;
-        parent::__construct(sprintf("Requested URL not found: %s", $url));
+        parent::__construct(sprintf("Requested URL not found: %s for HTTP verb %s", $url, isset($_REQUEST['x-verb']) ? $_REQUEST['x-verb'] : $_SERVER['REQUEST_METHOD']));
     }
 }
 
